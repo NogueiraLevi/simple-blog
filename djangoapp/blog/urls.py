e4796dc3221e9c410430123017c8a96b4e32f4c4
Levi <nogueiralevi400@gmail.com>
2025-06-03 17:27:34 -0300

    finished

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,19 +1,20 @@
-from blog.views import (category, created_by, page, post, tags, 
-                        search, PostListView, CreatedByListView) 
+from blog.views import (CategoryListView, CreatedByListView, PageDetailView,
+                        PostDetailView, PostListView, SearchListView,
+                        TagListView)
 from django.urls import path
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='index'),
-    path('post/<slug:slug>/', post, name='post'),
-    path('page/<slug:slug>/', page, name='page'),  
+    path('post/<slug:slug>/', PostListView.as_view(), name='post'),
+    path('page/<slug:slug>/', PageDetailView.as_view(), name='page'),  
     path(
         'created_by/<int:author_pk>/',
         CreatedByListView.as_view(),
         name='created_by'
     ),
-    path('category/<slug:slug>/', category, name='category'),
-    path('tags/<slug:slug>/', tags, name='tags'),
-    path('search/', search, name='search'),
+    path('category/<slug:slug>/', CategoryListView.as_view(), name='category'),
+    path('tags/<slug:slug>/', TagListView.as_view(), name='tags'),
+    path('search/', SearchListView.as_view(), name='search'),
 ]
