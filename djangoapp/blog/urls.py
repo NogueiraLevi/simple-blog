80f97116237b4613394cc6661930d7f27ccf0ca4
Levi <nogueiralevi400@gmail.com>
2025-06-03 11:46:59 -0300

    connections

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,14 +1,18 @@
 from blog.views import (category, created_by, page, post, tags, 
-                        search, PostListView) 
+                        search, PostListView, CreatedByListView) 
 from django.urls import path
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', PostListView.as_view(), name= 'index' ),
+    path('', PostListView.as_view(), name='index'),
     path('post/<slug:slug>/', post, name='post'),
-    path('page/<slug:slug/', page, name='page'),
-    path('created_by/<int:author_pk>/', created_by, name='created_by'),
+    path('page/<slug:slug>/', page, name='page'),  
+    path(
+        'created_by/<int:author_pk>/',
+        CreatedByListView.as_view(),
+        name='created_by'
+    ),
     path('category/<slug:slug>/', category, name='category'),
     path('tags/<slug:slug>/', tags, name='tags'),
     path('search/', search, name='search'),
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,26 +1,22 @@
+from typing import Any
+
 from blog.models import Post, Page
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
 from django.db.models import Q
-from django.http import Http404
-from django.shortcuts import render
+from django.db.models.query import QuerySet
+from django.http import Http404, HttpRequest, HttpResponse
+from django.shortcuts import redirect, render
 from django.views.generic.list import ListView
 
 PER_PAGE = 9
 
 
 class PostListView(ListView):
-    model = Post
     template_name = 'blog/pages/index.html'
     context_object_name = 'posts'
-    ordering = '-pk',
     paginate_by = PER_PAGE
     queryset = Post.objects.get_published()
-
-
-
-
-
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -59,6 +55,45 @@ def created_by(request, author_pk):
         }
     )
 
+
+class CreatedByListView(PostListView):
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self.temp_context: dict[str, Any] = {}
+    
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        user = self._temp_context['user']
+        user_full_name = user.username
+
+        if user.first_name:
+            user_full_name = f'{user.first_name} {user.last_name}'
+        page_title = user_full_name + 'Posts' + ' - '
+
+        ctx.update({
+            'page_title': page_title,
+        })
+
+        return ctx
+
+    def get_queryset(self) -> QuerySet[Any]:
+        qs = super().get_queryset
+        qs = qs.filter(created_by__pk=self.temp_context['user'].pk)
+        return qs
+
+    def get(self, request, *args, **kwargs):
+        author_pk = self.kwargs.get('author_pk')
+        user = User.objects.filter(pk=author_pk)
+
+        if user is None:
+            raise Http404()
+        
+        self._temp_context.update({
+            'author_pk': author_pk,
+            'user': user,
+        })
+
+        return super().get(request, *args, **kwargs)
 
 def category(request, slug):
     posts = Post.objects.get_published()\
